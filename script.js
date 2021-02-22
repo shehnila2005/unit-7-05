2224109f848d9bf8172dddf756c6103daad9ad57
Shehnila Azmat <shehnila.azmat@ocsbstudent.ca>
2021-02-22 23:57:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('button').addEventListener('click', movie)
+
+let age = 18
+let day = ''
+
+function movie () {
+  age = document.getElementById('input').value 
+  age = parseInt(age)
+  day = document.getElementById('day').value
+  if ((age < 19) && (day === 'monday' || day == 'friday')   ) {
+    
+    document.getElementById('answer').innerHTML = 'time for school'
+  } else if (( age >= 18) && (day === 'monday' || day === 'tuesday' || day === 'wednesday' || day === 'friday' || day === 'thursday')) {
+    
+    document.getElementById('answer').innerHTML = 'time for work'
+  } else if ((age >= 18) && (day === 'sunday' || day === 'saturday')) {
+    document.getElementById('answer').innerHTML = 'day off'
+ 
+  }
+  document.getElementById('movie').innerHTML = 'thank you'
+}
