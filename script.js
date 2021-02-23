37c244f5f399d91e3e6aaf755ea88e510aa17117
Shehnila Azmat <shehnila.azmat@ocsbstudent.ca>
2021-02-23 14:13:09 +0000

    blocked line

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function movie () {
   } else if ((age >= 18) && (day === 'monday' || day === 'tuesday' || day === 'wednesday' || day === 'friday' || day === 'thursday')) {
     document.getElementById('answer').innerHTML = 'time for work'
   } else if ((age >= 19) && (day === 'sunday' || day === 'saturday')) {
-  } else if ((age <= 19) && (day === 'sunday' || day === 'saturday')) {
+  } else if ((age <= 18) && (day === 'sunday' || day === 'saturday')) {
     document.getElementById('answer').innerHTML = 'yay weekend, no school!!'
   }
   document.getElementById('movie').innerHTML = 'thank you for verifying your age and todays date'
