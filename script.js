b59e1a4a21b9f86850cef65dd52ed5a5a5972a4e
Shehnila Azmat <shehnila.azmat@ocsbstudent.ca>
2021-02-23 00:26:25 +0000

    indentations

--- script.js
+++ script.js
@@ -4,17 +4,15 @@ let age = 18
 let day = ''
 
 function movie () {
-  age = document.getElementById('input').value 
+  age = document.getElementById('input').value
   age = parseInt(age)
   day = document.getElementById('day').value
 
-  if ((age < 19) && (day === 'monday' || day == 'friday')) {
-
+  if ((age < 19) && (day === 'monday' || day === 'friday')) {
     document.getElementById('answer').innerHTML = 'time for school'
-
     } else if (( age >= 18) && (day === 'monday' || day === 'tuesday' || day === 'wednesday' || day === 'friday' || day === 'thursday')) {
 
-    document.getElementById('answer').innerHTML = 'time for work'
+  document.getElementById('answer').innerHTML = 'time for work'
   } else if ((age >= 19) && (day === 'sunday' || day === 'saturday')) {
 
     } else if ((age <= 19) && (day === 'sunday' || day === 'saturday')) {
