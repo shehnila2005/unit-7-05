6e366bf074b9967b85deb221254f4a7135b022bb
Shehnila Azmat <shehnila.azmat@ocsbstudent.ca>
2021-02-23 00:37:44 +0000

    indentation and spacing - JavaScript

--- script.js
+++ script.js
@@ -10,9 +10,8 @@ function movie () {
 
   if ((age < 19) && (day === 'monday' || day === 'friday')) {
     document.getElementById('answer').innerHTML = 'time for school'
-  } else if(( age >= 18) && (day === 'monday' || day === 'tuesday' || day === 'wednesday' || day === 'friday' || day === 'thursday')) {
-
-    document.getElementById('answer').innerHTML = 'time for work'
+  } else if ((age >= 18) && (day === 'monday' || day === 'tuesday' || day === 'wednesday' || day === 'friday' || day === 'thursday')) {
+  document.getElementById('answer').innerHTML = 'time for work'
   } else if ((age >= 19) && (day === 'sunday' || day === 'saturday')) {
 
   } else if ((age <= 19) && (day === 'sunday' || day === 'saturday')) {
